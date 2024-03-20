970583d3bb95ac21fc01c07719457fd878e44566
masoudyeganeh <50057163+masoudyeganeh@users.noreply.github.com>
2024-03-20 04:10:04 +0330

    add compare values

--- testCase.py
+++ testCase.py
@@ -1,4 +1,4 @@
-#compare fapi keys with rest keys
+# compare fapi keys with rest keys
 def compareKeys(object):
     fapiKeys = list(dict.fromkeys(list(object.getFapiKeys())))
     restKeys = list(dict.fromkeys(list(object.getRestKeys())))
@@ -7,7 +7,8 @@ def compareKeys(object):
     else:
         return list(set(fapiKeys) - set(restKeys))
 
-#compare fapi values with rest values
+
+# compare fapi values with rest values
 def compareValues(object):
     fapiValues = list(dict.fromkeys(list(object.getFapiVales())))
     restValues = list(dict.fromkeys(list(object.getRestVales())))
